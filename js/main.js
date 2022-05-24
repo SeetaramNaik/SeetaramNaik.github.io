7ffa9369ac92efd070d4f1d915bf7d28ccd20fb1
Seetaram Naik <seetaramnaik44@gmail.com>
2022-05-24 21:49:15 +0530

    modified portfolio

--- js/main.js
+++ js/main.js
@@ -1,80 +1,80 @@
-(function ($) {
-  // "use strict";
+// (function ($) {
+//   // "use strict";
 
-  // Navbar on scrolling
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 700) {
-      $('.navbar').fadeIn('slow').css('display', 'flex');
-    } else {
-      $('.navbar').fadeOut('slow').css('display', 'none');
-    }
-  });
+//   // Navbar on scrolling
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 300) {
+//       $('.navbar').fadeIn('slow').css('display', 'flex');
+//     } else {
+//       $('.navbar').fadeOut('slow').css('display', 'none');
+//     }
+//   });
 
-  // Smooth scrolling on the navbar links
-  $('.navbar-nav a').on('click', function (event) {
-    if (this.hash !== '') {
-      event.preventDefault();
+//   // Smooth scrolling on the navbar links
+//   $('.navbar-nav a').on('click', function (event) {
+//     if (this.hash !== '') {
+//       event.preventDefault();
 
-      $('html, body').animate(
-        {
-          scrollTop: $(this.hash).offset().top - 45,
-        },
-        1500,
-        'easeInOutExpo'
-      );
+//       $('html, body').animate(
+//         {
+//           scrollTop: $(this.hash).offset().top - 45,
+//         },
+//         1500,
+//         'easeInOutExpo'
+//       );
 
-      if ($(this).parents('.navbar-nav').length) {
-        $('.navbar-nav .active').removeClass('active');
-        $(this).closest('a').addClass('active');
-      }
-    }
-  });
+//       if ($(this).parents('.navbar-nav').length) {
+//         $('.navbar-nav .active').removeClass('active');
+//         $(this).closest('a').addClass('active');
+//       }
+//     }
+//   });
 
-  // Typed Initiate
-  if ($('.typed-text-output').length == 1) {
-    var typed_strings = $('.typed-text').text();
-    var typed = new Typed('.typed-text-output', {
-      typeSpeed: 100,
-      backSpeed: 20,
-      smartBackspace: false,
-      loop: true,
-    });
-  }
+//   // Typed Initiate
+//   if ($('.typed-text-output').length == 1) {
+//     var typed_strings = $('.typed-text').text();
+//     var typed = new Typed('.typed-text-output', {
+//       typeSpeed: 100,
+//       backSpeed: 20,
+//       smartBackspace: false,
+//       loop: true,
+//     });
+//   }
 
-  // Scroll to Bottom
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('.scroll-to-bottom').fadeOut('slow');
-    } else {
-      $('.scroll-to-bottom').fadeIn('slow');
-    }
-  });
+//   // Scroll to Bottom
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 100) {
+//       $('.scroll-to-bottom').fadeOut('slow');
+//     } else {
+//       $('.scroll-to-bottom').fadeIn('slow');
+//     }
+//   });
 
-  // Skills
-  $('.skill').waypoint(
-    function () {
-      $('.progress .progress-bar').each(function () {
-        $(this).css('width', $(this).attr('aria-valuenow') + '%');
-      });
-    },
-    { offset: '80%' }
-  );
+//   // Skills
+//   $('.skill').waypoint(
+//     function () {
+//       $('.progress .progress-bar').each(function () {
+//         $(this).css('width', $(this).attr('aria-valuenow') + '%');
+//       });
+//     },
+//     { offset: '80%' }
+//   );
 
-  // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 200) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-    return false;
-  });
+//   // Back to top button
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 200) {
+//       $('.back-to-top').fadeIn('slow');
+//     } else {
+//       $('.back-to-top').fadeOut('slow');
+//     }
+//   });
+//   $('.back-to-top').click(function () {
+//     $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+//     return false;
+//   });
 
   
-})(jQuery);
+// })(jQuery);
 
 
 
